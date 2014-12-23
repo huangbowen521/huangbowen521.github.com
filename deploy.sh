92f13f7b9b425a0bb74df724aedc7216048c5416
Bowen <huangbowen521@gmail.com>
2014-12-23 11:48:45 +0800

    new post

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #! /bin/zsh
 ruby -v
-rake generate 
-rake sync_post
-rake deploy
-rake weibo
-rake linkedin
+/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/rake generate 
+/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/rake sync_post
+/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/rake deploy
+/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/rake weibo
+/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/rake linkedin
 git add -A
 
 git ci -m "$1"
